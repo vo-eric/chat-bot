d97c260633b5b9fbf997d3f241336957b886abaf
Eric Vo <vo.eric@gmail.com>
2025-06-09 20:29:48 -0400

    Add sample tools to route.ts

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -9,6 +9,7 @@ import {
   type Message,
 } from "ai";
 import { loadMessages, saveMessages } from "tools/chat-store";
+import z from "zod";
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
@@ -27,6 +28,32 @@ export async function POST(req: Request) {
     model: openai("gpt-4o"),
     system: "You are a helpful assistant.",
     messages,
+    tools: {
+      // server-side tool with execute function:
+      getWeatherInformation: {
+        description: "show the weather in a given city to the user",
+        parameters: z.object({ city: z.string() }),
+        execute: async ({}: { city: string }) => {
+          const weatherOptions = ["sunny", "cloudy", "rainy", "snowy", "windy"];
+          return weatherOptions[
+            Math.floor(Math.random() * weatherOptions.length)
+          ];
+        },
+      },
+      // client-side tool that starts user interaction:
+      askForConfirmation: {
+        description: "Ask the user for confirmation.",
+        parameters: z.object({
+          message: z.string().describe("The message to ask for confirmation."),
+        }),
+      },
+      // client-side tool that is automatically executed on the client:
+      getLocation: {
+        description:
+          "Get the user location. Always ask for confirmation before using this tool.",
+        parameters: z.object({}),
+      },
+    },
     async onFinish({ response }) {
       await saveMessages({
         id,
